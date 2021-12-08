f2ce33b9e17bb59ba96a0b4e024f718a63dd0015
cnarutox <cnarutox@gmail.com>
2021-12-07 16:09:36 -0800

    reschedule project .@cnarutox

--- src/Authoring.java
+++ src/Authoring.java
@@ -1,8 +1,10 @@
 import java.awt.*;
 
 import javax.swing.*;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
+import javax.swing.event.*;
+import java.awt.event.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 class Authoring extends JFrame {
 
@@ -10,18 +12,43 @@ class Authoring extends JFrame {
         super(s);
     }
 
+    static class ImportVideo extends JPanel {
+        JLabel label = new JLabel("Imported Video Path");
+        JTextField videoNameField = new JTextField(25);
+        JButton viewBtn = new JButton("View");
+
+        public ImportVideo() {
+            super();
+            this.add(label);
+            this.add(videoNameField);
+            this.add(viewBtn);
+            viewBtn.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    JFileChooser fc = new JFileChooser(
+                            "C:/Users/cwx/OneDrive - University of Southern California/CSCI576/Project");
+                    fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                    int val = fc.showOpenDialog(null); // 文件打开对话框
+                    if (val == JFileChooser.APPROVE_OPTION) {
+                        videoNameField.setText(fc.getSelectedFile().toString());
+                    } else {
+                        // videoName.setText("未选择文件");
+                    }
+                }
+            });
+        }
+
+    }
+
     public static void main(String[] args) {
         Authoring authoring = new Authoring("HyperLinked Video Authoring Tool");
         authoring.setLayout(new BorderLayout());
 
-        JPanel player = new JPanel();
-        player.add(new JLabel(
-                "Import Video"));
-        authoring.getContentPane().add(player, BorderLayout.NORTH);
+        ImportVideo importPanel = new ImportVideo();
+        authoring.getContentPane().add(importPanel, BorderLayout.NORTH);
 
         VideoPlayer videoPlayer = new VideoPlayer();
         videoPlayer.setBackground(Color.BLUE);
-        videoPlayer.playVideo();
         authoring.getContentPane().add(videoPlayer, BorderLayout.CENTER);
 
         JPanel frameSlider = new JPanel();
